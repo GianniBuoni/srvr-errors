e245ed49eb830106c8a32c72b53a6b88c5619376
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2026-03-03 13:12:21 -0500

    fix: add missing build-inputs and tasks

--- nix/build-inputs.nix
+++ nix/build-inputs.nix
@@ -1,6 +1,27 @@
 # module to define build inputs for the shells and nix-build
 {moduleWithSystem, ...}: {
   flake.aspects.devshells.build = moduleWithSystem ({pkgs, ...}: {
-    packages = with pkgs; [pkg-config];
+    packages = with pkgs; [sqlx-cli postgresql pkg-config];
+
+    env = [
+      {
+        name = "PGDATA";
+        value = ".postgres";
+      }
+    ];
+
+    commands = [
+      {
+        name = "enterTest";
+        help = "Test build shell has all necessary tooling";
+        command = ''
+          cargo -V
+          cargo clippy -V
+          just -V
+          postgres -V
+          sqlx -V
+        '';
+      }
+    ];
   });
 }
