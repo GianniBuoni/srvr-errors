abd70d5ee5040b8c6f92e568748e473b9a1d6a84
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2026-03-03 13:25:15 -0500

    ci: add workflows (#1)
    * ci: add workflows
    
    * fix: add missing build-inputs and tasks
    
    * fix: use correct ci task in test workflow

--- nix/devshell-dev.nix
+++ nix/devshell-dev.nix
@@ -1,6 +1,6 @@
 # module to define shell configurations exclusive to the development shell
 {moduleWithSystem, ...}: {
   flake.aspects.devshells.dev = moduleWithSystem ({pkgs, ...}: {
-    packages = with pkgs; [sqlx-cli commitizen rust-analyzer];
+    packages = with pkgs; [commitizen rust-analyzer];
   });
 }
